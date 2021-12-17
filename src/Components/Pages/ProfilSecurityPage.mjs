2682457c78cf7e13d8fd2c8e5f92a492da6d9580
Kieran <kieranvm@gmail.com>
2021-12-17 12:44:38 +0100

    Change into module and minore fixes

--- src/Components/Pages/ProfilSecurityPage.mjs
+++ src/Components/Pages/ProfilSecurityPage.mjs
@@ -1,69 +1,11 @@
-import { Redirect } from "../Router/Router";
-import { getSessionObject } from "../../utils/session";
+import { Redirect } from "../Router/Router.js";
+import { getSessionObject } from "../../utils/session.js";
 import "../../stylesheets/profileStyle.css";
-
-/**
- * Render the ProfilPage
- */
-
-const ProfilSecurityPage = async () => {
-	const images = importAll(require.context('../../img/users', false, /\.(png|jpe?g|svg)$/));
-
-	let userEmail = getSessionObject("user");
-	if(! userEmail) return Redirect("/login");
-
-	// Collecting the info of the user
-	try{
-		const response = await fetch("/api/users/"+userEmail.email);
-
-		if(!response.ok){
-			throw new Error(
-				"fetch error : " + Response.status + " : " + response.statusText
-			);
-		}
-
-		const user = await response.json();
-	
-
-		// reset #page div
-		const pageDiv = document.querySelector("#page");
-		pageDiv.innerHTML = "";
-
-		const structure = document.createElement("section");
-
-		addTabContent(user, structure, images);
-
-		pageDiv.appendChild(structure);
-	} catch (error){
-		console.error("ProfilView::error: " + error);
-	}
-};
-
-function addTabContent(user, structure, images){
-	structure.className = "py-5 my-5";
-
-	const container = document.createElement("div");
-	container.classList.add("container");
-
-	const title = document.createElement("h1");
-	title.className = "mb-5";
-	title.textContent = "Welcome, " + user.firstname;
-
-	const account = document.createElement("div");
-	account.className = "shadow rounded-lg d-block d-sm-flex";
-
-	container.appendChild(title);
-	
-	addChoiceNav(account, user, images);
-
-    addInfoNav(account, user);
+import { addInfoContent, addNavActive, addNavInactive, emptyErrorMessage, errorMessage } from "./ProfilPage.js";
 
 
-	container.appendChild(account);
-	structure.appendChild(container);
-}
 
-function addChoiceNav(account, user, images){
+export function addSecurityChoiceNav(account, user, images){
 	const choices = document.createElement("div");
 	choices.className = "profile-tab-nav border-right";
 
@@ -74,8 +16,9 @@ function addChoiceNav(account, user, images){
 	usersPicture.className = "img-circle text-center mb-3";
 
 	const picture = document.createElement("img");
-	picture.src = images[user.profil_picture];
+	picture.src = images;
 	picture.alt = "User\'s picture";
+	picture.id = "UsersPicture";
 
 	const userName = document.createElement("h4");
 	userName.className = "text-center";
@@ -105,7 +48,7 @@ function addChoiceNav(account, user, images){
 	account.appendChild(choices);
 }
 
-function addInfoNav(account, user){
+export function addSecurityInfoNav(account, user){
 	const infoTop = document.createElement("div");
 	infoTop.className = "tab-content p-4 p-md-5";
 
@@ -242,118 +185,3 @@ async function onCancel(e){
 	document.getElementById("Nouveau mot de passe").value = "";
 	document.getElementById("Confirmer le nouveau mot de passe").value = "";
 }
-
-function addInfoContent(row, content, contentName){
-	const info = document.createElement("div");
-	info.className = "col-md-6";
-
-	const textContent = document.createElement("div");
-	textContent.className = "form-group";
-
-	const label = document.createElement("label");
-	label.innerText = contentName;
-
-	const input = document.createElement("input");
-	input.id = contentName;
-	input.type = "password";
-	input.className = "form-control";
-	input.value = content;
-	input.setAttribute("content", content);
-
-
-	textContent.appendChild(label);
-	textContent.appendChild(input);
-	info.appendChild(textContent);
-
-	row.appendChild(info);
-}
-
-function addNavActive(nameNav, namePage, destination){
-	const nav = document.createElement("a");
-	nav.className="nav-link active";
-	nav.id = nameNav+"-tab";
-	nav.setAttribute("data-toggle", "pill");
-	nav.href = "#";
-	nav.setAttribute("role", "tab");
-	nav.setAttribute("aria-controls", nameNav);
-	nav.setAttribute("aria-selected", "false");
-
-	const name = document.createElement("i");
-	if (nameNav === "Profil"){
-		name.className = "fa fa-home text-center mr-1";
-	}else if(nameNav === "Crédits"){
-		name.className = "fa fa-dollar-sign text-center mr-1";
-	}else if(nameNav === "Historique d'enchères"){
-		name.className = "fa fa-history text-center mr-1";
-	}else if(nameNav === "Sécurité"){
-		name.className = "fa fa-lock text-center mr-1";
-	}else if(nameNav === "Adresse"){
-		name.className = "fa fa-home text-center mr-1";
-	}
-	
-
-	nav.appendChild(name);
-	nav.innerHTML += " " + nameNav;
-
-	destination.appendChild(nav);
-}
-
-function addNavInactive(nameNav, namePage, destination){
-	const navLi = document.createElement("li");
-	navLi.className = "nav-item";
-
-	const nav = document.createElement("a");
-	nav.className="nav-link";
-	nav.id = nameNav+"-tab";
-	nav.setAttribute("data-toggle", "pill");
-	nav.href = "/profil/"+namePage;
-	nav.setAttribute("role", "tab");
-	nav.setAttribute("aria-controls", nameNav);
-	nav.setAttribute("aria-selected", "false");
-
-	const name = document.createElement("i");
-	if (nameNav === "Profil"){
-		name.className = "fa fa-home text-center mr-1";
-	}else if(nameNav === "Crédits"){
-		name.className = "fa fa-dollar-sign text-center mr-1";
-	}else if(nameNav === "Historique d'enchères"){
-		name.className = "fa fa-history text-center mr-1";
-	}else if(nameNav === "Sécurité"){
-		name.className = "fa fa-lock text-center mr-1";
-	}else if(nameNav === "Adresse"){
-		name.className = "fa fa-home text-center mr-1";
-	}
-	
-
-	nav.appendChild(name);
-	nav.innerHTML += " " + nameNav;
-
-	navLi.appendChild(nav);
-	destination.appendChild(navLi);
-}
-
-function importAll(r) {
-	let images = {};
-	r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
-	return images;
-}
-
-function errorMessage(message) {
-	const alertDiv = document.getElementById("message");
-	alertDiv.innerHTML=
-	'<br><div class="alert alert-danger" role="alert" id="message">  Attention : '+ message  + ' </div>';
-}
-
-function notificationMessage(message) {
-	const alertDiv = document.getElementById("message");
-	alertDiv.innerHTML=
-	'<br><div class="alert alert-success" role="alert" id="message">'+ message  + ' </div>';
-}
-
-function emptyErrorMessage(){
-	const alertDiv = document.getElementById("message");
-	alertDiv.innerHTML= '<div id="message"></div>';
-}
-
-
-export default ProfilSecurityPage;
