a61b9a4152e37b1415306ce2cff542574cc4dfb1
ZERUNEmil <emil.zerun@student.vinci.be>
2021-12-18 18:39:26 +0100

    Afficher annonce (no api)

--- src/Components/Pages/AnnoncesPage.js
+++ src/Components/Pages/AnnoncesPage.js
@@ -3,49 +3,41 @@
  */
  import {Redirect} from "../Router/Router";
  import Navbar from "../Navbar/Navbar";
-import starryNightImage from "../../img/StarryNight.jpg";
-import criImage from "../../img/cri.jpg";
-import soleilImage from"../../img/soleil.jpg";
+import { Auctions } from "../../../../projet_js_backend/model/auctions";
+
 
 
 const AnnoncesPage = () => {
 	const pageDiv = document.querySelector("#page");
+
+  const activeAuctions = await fetch ("/api/annonces/getAllActive");
+  if(!activeAuctions.ok)alert("Erreur lors du get d'annonces en cours")
+
+
 	pageDiv.innerHTML = `<br>
 	<div class="row">		
 	<div class="column">
-    <img src=${starryNightImage}>
-    <img src=${criImage}>
-    <img src=${soleilImage}>
-    <img src=${starryNightImage}>
-    <img src=${soleilImage}>
-    <img src=${criImage}>
-    <img src=${starryNightImage}>
+    <img src="https://i.ibb.co/wCbQPWM/veil.png">
+    <img src="https://i.ibb.co/kKfRzVr/diner.jpg">
+    <img src="https://i.ibb.co/QNvvyQX/pearl.jpg">
   </div>
   <div class="column">
-    <img src=${soleilImage}>
-    <img src=${starryNightImage}>
-    <img src=${soleilImage}>
-    <img src=${starryNightImage}>
-    <img src=${criImage}>
-    <img src=${soleilImage}>
+    <img src="https://i.ibb.co/wCbQPWM/veil.png">
+    <img src="https://i.ibb.co/kKfRzVr/diner.jpg">
+    <img src="https://i.ibb.co/QNvvyQX/pearl.jpg">
   </div>
   <div class="column">
-    <img src=${soleilImage}>
-    <img src=${criImage}>
-    <img src=${soleilImage}>
-    <img src=${starryNightImage}>
-    <img src=${soleilImage}>
-    <img src=${criImage}>
-    <img src=${starryNightImage}>
+    <img src="https://i.ibb.co/wCbQPWM/veil.png">
+    <img src="https://i.ibb.co/kKfRzVr/diner.jpg">
+    <img src="https://i.ibb.co/QNvvyQX/pearl.jpg">
   </div>
   <div class="column">
-    <img src=${soleilImage}>
-    <img src=${starryNightImage}>
-    <img src=${soleilImage}>
-    <img src=${starryNightImage}>
-    <img src=${criImage}>
-    <img src=${soleilImage}>
+    <img src="https://i.ibb.co/wCbQPWM/veil.png">
+    <img src="https://i.ibb.co/kKfRzVr/diner.jpg">
+    <img src="https://i.ibb.co/QNvvyQX/pearl.jpg">
   </div>
+
+
 	
 	`;
 
--- src/Components/Pages/HomePage.js
+++ src/Components/Pages/HomePage.js
@@ -3,52 +3,39 @@
  */
  import {Redirect} from "../Router/Router";
  import Navbar from "../Navbar/Navbar";
-import starryNightImage from "../../img/StarryNight.jpg";
-import criImage from "../../img/cri.jpg";
-import soleilImage from"../../img/soleil.jpg";
+/*const  { rows } = await pool.query('SELECT * FROM project.user WHERE id_user = $1', [id]);
+if (! rows) return;
+return rows[0];
+*/
 
 const HomePage = () => {
 	const pageDiv = document.querySelector("#page");
-	pageDiv.innerHTML = `<br><h1> All'Astra <h1>
-	<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-  <ol class="carousel-indicators">
-    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-  </ol>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img class="d-block w-100" src="${starryNightImage}" alt="First slide" style="max-width: 900px; max-height: 700px; margin : auto">
-      <div class="carousel-caption d-none d-md-block">
-      <h5>Starry Night</h5>
-      <p>54132€</p>
-    </div>
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="${criImage}" alt="Second slide" style="max-width: 900px; max-height: 700px; margin : auto">
-      <div class="carousel-caption d-none d-md-block">
-      <h5>Le Cri</h5>
-      <p>9999€</p>
-    </div>
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="${soleilImage}" alt="Third slide" style="max-width: 900px; max-height: 700px; margin : auto">
-      <div class="carousel-caption d-none d-md-block">
-      <h5>1 2 3 Soleil</h5>
-      <p>56€</p>
-    </div>
-    </div>
-  </div>
-  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
+	pageDiv.innerHTML = `
+  
+  <br><h1> All'Astra <h1>
+  <div class="container">
+
+
+<div style="padding:25px;  background:#9D4F96; border-radius:25px; margin:200px " >
+<h1>Les enchères les plus actives du moment</h1>
+</div>
+
+<div class="row">		
+<div class="columnHome">
+  <img src="https://i.ibb.co/wCbQPWM/veil.png">
+</div>
+<div class="columnHome">
+  <img src="https://i.ibb.co/kKfRzVr/diner.jpg">
+</div>
+<div class="columnHome">
+  <img src="https://i.ibb.co/QNvvyQX/pearl.jpg">
+</div>
+<div class="columnHome">
+  <img src="https://i.ibb.co/QNvvyQX/pearl.jpg">
 </div>
 `;
+
 };
+
 
 export default HomePage;
